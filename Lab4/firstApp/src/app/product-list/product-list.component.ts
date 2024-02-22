e03183e9232129f72627d4e46188bfbdeae5e81d
dan-chik <123585641+dan-chik@users.noreply.github.com>
2024-02-22 11:01:37 +0600

    lab4

--- Lab4/firstApp/src/app/product-list/product-list.component.ts
+++ Lab4/firstApp/src/app/product-list/product-list.component.ts
@@ -9,10 +9,14 @@ import { Product, products } from '../products';
 export class ProductListComponent {
   products = [...products];
 
-  share() {
+  share(product: Product) {
     //window.alert('The product has been shared!');
-    const telegramLink = `https://telegram.me/share/url?url=<http://localhost:4200/products/1>`;
-    window.open(telegramLink, '_blank ');
+    //const telegramLink = `https://telegram.me/share/url?url=<http://localhost:4200/products/1>`;
+    //window.open(telegramLink, '_blank ');
+    const telegramMessage = `Check out this product: ${product.name} - ${product.description} - ${product.rating} Stars - ${product.kaspiLink}`;
+    const telegramURL = `https://t.me/share/url?url=${encodeURIComponent(product.kaspiLink)}&text=${encodeURIComponent(telegramMessage)}`;
+
+    window.location.href = telegramURL;
   }
 
   onNotify() {
