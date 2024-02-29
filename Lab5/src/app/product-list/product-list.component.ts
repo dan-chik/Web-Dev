63fa07651dfac511a80bd614b3f116a7ce824d2b
dan-chik <123585641+dan-chik@users.noreply.github.com>
2024-02-29 10:14:46 +0600

    Lab 5

--- Lab5/src/app/product-list/product-list.component.ts
+++ Lab5/src/app/product-list/product-list.component.ts
@@ -8,6 +8,12 @@ import { Product, products } from '../products';
 })
 export class ProductListComponent {
 
+  products: Product[] = [...products];
+  categories = ['Earings', 'Ring', 'Necklace', 'Bracelet'];
+  selectedCategory: string | undefined;
+  selectedCategoryProduct: Product | undefined;
+  filteredProducts: Product[] = [];
+  
   share(product: Product) {
     const telegramMessage = `Check out this product: ${product.name} - ${product.description} - ${product.rating} Stars - ${product.kaspiLink}`;
     const telegramURL = `https://t.me/share/url?url=${encodeURIComponent(product.kaspiLink)}&text=${encodeURIComponent(telegramMessage)}`;
@@ -19,14 +25,23 @@ export class ProductListComponent {
     window.alert('You will be notified when the product goes on sale');
   }
 
-  products: Product[] = [...products];
-  categories = ['Earings', 'Ring', 'Necklace', 'Bracelet'];
-  selectedCategory: string | undefined;
-  filteredProducts: Product[] = [];
-
   onSelectCategory(category: string): void {
     this.selectedCategory = category;
+    this.selectedCategoryProduct = this.products.find(product => product.category === category);
     this.filterProductsByCategory();
+
+    // Create a placeholder product for the selected category
+    this.selectedCategoryProduct = {
+      id: -1, // Use a unique identifier for the placeholder
+      name: category,
+      price: 0, // You can set a default price or any other placeholder values
+      description: 'Category details', // You can set a default description
+      image: '', // You can set a default image URL or leave it empty
+      rating: 0, // You can set a default rating or leave it as 0
+      kaspiLink: '', // You can set a default link or leave it empty
+      likes: 0, // You can set a default number of likes or leave it as 0
+      category: category,
+    };
   }
 
   onRemoveProduct(product: Product): void {
