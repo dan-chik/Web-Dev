40a14d34791120fcfc5143532393f5c77c61d988
dan-chik <123585641+dan-chik@users.noreply.github.com>
2024-04-04 11:07:18 +0500

    new lab8

---
+++ lab8_2/shop_back/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from api.views import get_categories, get_category, get_products, get_product,get_products_by_category, CategoryProducts
+
+urlpatterns = [
+    path('categories/', get_categories),
+    path('categories/<int:pk>/', get_category),
+    path('products/', get_products),
+    path('products/<int:pk>/', get_product),
+    path('categories/<int:pk>/products/', CategoryProducts.as_view()),
+
+]
---
+++ lab8_2/shop_back/api/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http.response import HttpResponse, JsonResponse
+from api.models import Category, Product
+
+
+def get_products(request):
+    products = Product.objects.all()
+    products_json = [product.to_json() for product in products]
+
+    return JsonResponse(products_json, safe=False)
+
+def get_product(request, pk=None):
+    try:
+        product = Product.objects.get(id=pk)
+        return JsonResponse(product.to_json())
+    except Product.DoesNotExist as e:
+        return JsonResponse({
+            'error': str(e)
+        })
+
+def get_products_by_category(request, pk=None):
+    try:
+        category = Category.objects.get(pk=pk)
+        products = Product.objects.filter(category=category)
+        return JsonResponse(products.to_json())
+    except Product.DoesNotExist as e:
+        return JsonResponse({
+            'error': str(e)
+        })
+
+def get_categories(request):
+    categories = Category.objects.all()
+    categories_json = [category.to_json() for category in categories]
+
+    return JsonResponse(categories_json, safe=False)
+
+def get_category(request, pk=None):
+    try:
+        category = Category.objects.get(id=pk)
+        return JsonResponse(category.to_json())
+    except Category.DoesNotExist as e:
+        return JsonResponse({
+            'error': str(e)
+        })
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+
+class CategoryProducts(APIView):
+    def get(self, request, category_id):
+        products = Product.objects.filter(category_id=category_id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
